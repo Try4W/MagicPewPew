3ab8db04e8237e92fabc91eea3c7cb48d77a7c3e
Try4W <alexandr2levin@gmail.com>
2015-07-08 15:16:52 +0400

    Rewrited client-server message system

--- MagicPewPew/messager.py
+++ MagicPewPew/messager.py
@@ -1,15 +1,17 @@
 import socket
 import struct
 
-__author__ = 'Someone from the internet'
+__author__ = 'Alexandr'
 
-def send_msg(channel, message):
+
+def _send_msg(channel, message):
     try:
         channel.send(struct.pack("i", len(message)) + message.encode("utf-8"))
     except OSError as e:
         print(e)
 
-def recv_msg(channel):
+
+def _recv_msg(channel):
     try:
         size = struct.unpack("i", channel.recv(struct.calcsize("i")))[0]
         data = ""
@@ -21,3 +23,16 @@ def recv_msg(channel):
         return data.strip()
     except OSError as e:
         print(e)
+
+
+def send_msg(channel, message):
+    channel.send(pack_data(message))
+
+
+def recv_msg(channel):
+    message_size = struct.unpack("i", channel.recv(4))[0]  # First four bytes
+    return struct.unpack("%is" % message_size, channel.recv(message_size))[0].decode("utf-8")
+
+def pack_data(data):
+    data_len = len(data)
+    return struct.pack("i%is" % data_len, data_len, data.encode("utf-8"))
